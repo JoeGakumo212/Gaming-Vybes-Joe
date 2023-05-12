ecda7c2f5dec91f17c002f326b1a2ec405d1f509
JoeGakumo212 <joegakumo2@gmail.com>
2023-05-12 17:26:21 +0300

    initial file creation

---
+++ components/Navbar.js
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+export default function Navbar() {
+  return (
+    <div className="container">
+      <nav className='navbar'>
+        <Image src="/rupee.png" width={50} height={48} />
+        <h1 className='joe'>JoeGakumo Vybes</h1>
+        <ul>
+          <li><Link href="/">Home</Link></li>
+          <li><Link href="/guides">Guides</Link></li>
+        </ul>
+      </nav>
+      <div className="banner">
+        <Image src="/banner.png" width={966} height={276} />
+      </div>
+    </div>
+  )
+}
---
+++ components/footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (  
+        <><div className="container">
+        <h1 className="joe1">CopyRight@ 2023 | JoeGakumo</h1>
+        </div>
+        </>
+    );
+}
+ 
+export default Footer;
