71e5aa8773e892054805c7e8637e4c306cde2bfe
JoeGakumo212 <joegakumo2@gmail.com>
2023-05-12 17:43:16 +0300

    minor changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -13,7 +13,7 @@ export default function Navbar() {
         </ul>
       </nav>
       <div className="banner">
-        <Image src="/banner.png" width={966} height={276} />
+        <Image src="/banner.png" width={1800} height={276} />
       </div>
     </div>
   )
